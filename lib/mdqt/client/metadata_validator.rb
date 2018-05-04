b521f4b57aab048447d1ac188fc0cf61a0729454
Pete Birkinshaw <pete@binary-ape.org>
2018-05-04 06:53:13 +0100

    Placeholder for Validator code

---
+++ lib/mdqt/client/metadata_validator.rb
@@ -0,0 +1,23 @@
+module MDQT
+  class Client
+
+    class MetadataValidator
+
+      def initialize(response)
+
+        puts response.inspect
+
+      end
+
+
+      def ok?
+        true
+      end
+
+
+
+    end
+
+  end
+
+end
