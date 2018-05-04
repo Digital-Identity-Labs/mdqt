a6f0b2bc850f1bfe0609e57877f0329aa56f49d1
Pete Birkinshaw <pete@binary-ape.org>
2018-05-04 06:54:26 +0100

    Refactor Get command

--- lib/mdqt/cli/get.rb
+++ lib/mdqt/cli/get.rb
@@ -8,24 +8,58 @@ module MDQT
 
       def run(args, options)
 
+        args = absorb_piped_args(args) if options.stdin
+
+        abort("Please specify --all if you wish to request all entities from #{options.service}") if args.empty? && !options.all
+
+        results = get_results(args, options)
+
+        #results = MetadataValidator.validate_responses(results) if options.validate
+
+        #results = MetadataAggregator.aggregate_responses(results) if options.aggregate
+
+        output_metadata(results, options)
+
+      end
+
+      def get_results(args, options)
+
         client = MDQT::Client.new(base_url: options.service)
 
-        if args.empty?
+        args.empty? ? [client.get_metadata("")] : args.collect {|entity_id| client.get_metadata(entity_id)}
 
-          if options.all
-            results = [client.get_metadata("")]
-          else
-            abort("Please specify --all if you wish to request all entities from #{options.service}")
-          end
+      end
 
+      def absorb_piped_args(args)
+        args
+      end
+
+      def action(results, options)
+        case
+        when options.save
+          :save_files
         else
-          results = args.collect {|entity_id| client.get_metadata(entity_id)}
+          :print_to_stdout
         end
+      end
 
-        results.each do |result|
-          puts result.body
+      def output_metadata(results, options)
+        case action(results, options)
+        when :save_files
+          output_files(results, options)
+        when :print_to_stdout
+          output_to_stdout(results, options)
+        else
+          abort "Error - can't determine output type"
         end
+      end
 
+      def output_to_stdout(results, options)
+        results.each {|r| puts r.data + "\n"}
+      end
+
+      def output_files(results, options)
+        abort "Unimplemented feature"
       end
 
     end
