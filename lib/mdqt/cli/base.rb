6fa6685b5ee2c4c3c53913bc06602d6a02eee930
Pete Birkinshaw <pete@binary-ape.org>
2018-05-12 15:19:21 +0100

    Spaced

--- lib/mdqt/cli/base.rb
+++ lib/mdqt/cli/base.rb
@@ -25,7 +25,7 @@ module MDQT
           STDERR.puts "Using #{options.service}"
           STDERR.puts "Caching is #{options.cache ? 'on' : 'off'}"
           STDERR.print "Signature verification is #{MDQT::Client.verification_available? ? 'available' : 'not available'}"
-          STDERR.puts  "#{options.verify_with ? " and active" : "but inactive "} for this request" if MDQT::Client.verification_available?
+          STDERR.puts  " #{options.verify_with ? "and active" : "but inactive"} for this request" if MDQT::Client.verification_available?
           STDERR.puts
         end
       end
